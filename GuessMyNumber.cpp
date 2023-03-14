f9ef56468fb73499ffe5b9f6bde5763b54a2e98f
LuigiTan <gibran.garcia@uniat.edu.mx>
2023-03-14 08:38:18 -0600

    Modificacion

--- GuessMyNumber.cpp
+++ GuessMyNumber.cpp
@@ -1,19 +1,84 @@
-// GuessMyNumber.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
-
+#include <Windows.h>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(NULL));//Se utiliza el tiempo de la maquina (reloj interno) para siempre generar un numero diferente 
+    int NumRan = rand() % 100;
+    int intentos = 0;
+    int Respuesta = 0;
+    int RangoMax = NumRan + 5;
+    int RangoMin = NumRan - 5;
+    cout << "Bienvenido al *Guess My Numer*\n";
+
+    do
+    {
+
+        cout << "Introduzca el numero que cree que la maquina haya elegido del 0 al 100\n";
+        cout << "Modo Test: El numero random es: " << NumRan << endl;
+        cin >> Respuesta;
+        if (Respuesta > NumRan)
+        {
+            intentos = intentos + 1;
+
+            cout << "Tu numero es mas alto que el de la maquina\n";
+            system("Color 09");
+            for (int i = RangoMax; i >= NumRan; i--)
+            {
+                if (Respuesta == i)
+                {
+                    cout << "Estas muy cerca, sigue intentandolo" << endl;
+                }
+            }
+
+        }
+        else if (Respuesta < NumRan)
+        {
+            intentos = intentos + 1;
+            cout << "Tu numero es mas bajo que el de la maquina\n";
+            system("Color 0C");
+            for (int i = RangoMin; i <= NumRan; i++)
+            {
+                if (Respuesta == i)
+                {
+                    cout << "Estas muy cerca, sigue intentandolo" << endl;
+                }
+            }
+
+        }
+        else if (Respuesta == NumRan)
+        {
+
+            intentos = intentos + 1;
+            if (intentos == 1)
+            {
+                cout << "FELICIDADES, encontraste el numero en el primer intento!!!!!";
+                system("Color 01");
+                Sleep(250);
+                system("Color 04");
+                Sleep(250);
+                system("Color 02");
+                Sleep(250);
+                system("Color 01");
+                Sleep(250);
+                system("Color 04");
+                Sleep(250);
+                system("Color 02");
+                Sleep(250);
+
+
+            }
+            else
+            {
+                system("Color 02");
+                cout << "Correcto! Tu numero de intentos fue de " << intentos << " Intentos";
+            }
+
+        }
+        else
+        {
+            cout << "Opcion no valida";
+        }
+    } while (Respuesta != NumRan);
+
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
